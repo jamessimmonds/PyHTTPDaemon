b32eb79febd761d221760184aeab4218dac410bc
James Simmonds <james.e.simmonds@gmail.com>
2020-12-06 16:44:36 +0000

    Wrapped daemon in class

--- src/httpdaemon/daemon.py
+++ src/httpdaemon/daemon.py
@@ -4,43 +4,56 @@ import re
 from response import htmlresponse, filenotfound, message, cssresponse
 from request import HttpRequest
 
-def runserver(
-    port=8000,
-    routes={
-        '/': lambda req : message("Daemon", "The HTTP daemon is running."),
-    },
-    host='127.0.0.1'
-    ):
+class Daemon:
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.bind((host, port))
-        sock.listen()
-        print(f"Listening on port {port}...")
-        
-        while True:
-        
-            conn, addr = sock.accept()
-            
-            # Receive request and parse to determine path
-            request = HttpRequest(conn.recv(1024))
+    def __init__(
+        self,
+        port=8000,
+        routes={
+            '/': lambda req : message("Daemon", "The HTTP daemon is running."),
+        },
+        host='127.0.0.1'
+        ):
 
-            print(request.requestline)
+        self.port = port
+        self.routes = routes
+        self.host = host
 
-            # Show the path accessed by the request
-            path = request.path
+    def runserver(self):
+
+        port = self.port
+        routes = self.routes
+        host = self.host
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             
-            # Send an HTTP response on the socket
-            if path in routes.keys():
-                conn.sendall(routes[path](request))
-            else:
-                errormessage = f"You have accessed the following path: {path}"
-                conn.sendall(filenotfound(errormessage))
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((host, port))
+            sock.listen()
+            print(f"Listening on port {port}...")
             
-            conn.close()
-        
-        sock.close()
+            while True:
+            
+                conn, addr = sock.accept()
+                
+                # Receive request and parse to determine path
+                request = HttpRequest(conn.recv(1024))
+
+                print(request.requestline)
+
+                # Show the path accessed by the request
+                path = request.path
+                
+                # Send an HTTP response on the socket
+                if path in routes.keys():
+                    conn.sendall(routes[path](request))
+                else:
+                    errormessage = f"You have accessed the following path: {path}"
+                    conn.sendall(filenotfound(errormessage))
+                
+                conn.close()
+            
+            sock.close()
 
 if __name__ == "__main__":
 
@@ -61,4 +74,5 @@ if __name__ == "__main__":
         '/query': lambda req : message('Query', f'Your query was {req.params}')
     }
 
-    runserver(routes=routes)
+    d = Daemon(routes=routes)
+    d.runserver()
